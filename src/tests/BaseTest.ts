4a98e741f8238e6c85b7939338963db20021cd4b
raman.kapur <raman.kapur@monobank.ua>
2026-07-21 21:49:20 +0300

    прибрав коммент

--- src/tests/BaseTest.ts
+++ src/tests/BaseTest.ts
@@ -10,8 +10,8 @@ export const test = baseTest.extend<MyFixtures>({
   searchPage: async ({ page }, use) => {
     // Автоматично створюємо об'єкт сторінки
     const searchPage = new GoogleSearchPage(page);
-    // Передаємо його в тест
     await use(searchPage);
+
   },
 });
 
